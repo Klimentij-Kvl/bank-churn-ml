bddcb4ad1b2f0b72ae5da9600d9b430c33826e45
Klimentij <klimentijkvlvsk@gmail.com>
2026-05-31 10:12:44 +0300

    Added requesrt and response classes

--- api/app.py
+++ api/app.py
@@ -2,10 +2,13 @@ from fastapi import FastAPI
 import joblib
 import pandas as pd
 from pydantic import BaseModel
+import logging
+
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
-model = joblib.load('../models/catboost_bank_churn.joblib')
+model = joblib.load('models/catboost_bank_churn.joblib')
 
 class PredictRequest(BaseModel):
     CreditScore: int
@@ -13,18 +16,28 @@ class PredictRequest(BaseModel):
     Gender: str
     Age: int
     Tenure: int
-    Balance: int
+    Balance: float
     NumOfProducts: int
     HasCrCard: int
     IsActiveMember: int
-    EstimatedSalary: int
+    EstimatedSalary: float
+
+class PredictResponse(BaseModel):
+    churn_probability: float
+    will_churn: bool
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
 
 @app.post('/predict')
-def predict(data: dict):
-    df = pd.DataFrame([data])
-    proba = model.predict_proba(df)[0][1]
+def predict(data: PredictRequest):
+    try:
+        df = pd.DataFrame([data.model_dump()])
+        proba = model.predict_proba(df)[0][1]
 
-    return {
-        "probability": float(proba),
-        "will_leave": bool(proba > 0.5)
-    }
+        return PredictResponse(churn_probability=round(float(proba), 4), will_churn=proba > 0.5)
+    except Exception as e:
+        logger.error(f"failed to load model: {e}")
+        raise
+    
